99ac70cae771aa8e4a76f1f8e32039b22d1eb6fa
Zhong Wang <zw355@cbsudanko.tc.cornell.edu>
2016-10-03 15:40:43 -0400

    tfbs.plotEnrichement interface has been chancged.

--- test/rtfbs.plotEnrichment.test.R
+++ test/rtfbs.plotEnrichment.test.R
@@ -1,12 +1,17 @@
 load("motif_intersected_sorted_uniq_sortedByAbsLogRatio.rdata")
 
-library(rtfbs_db);
+library(rtfbsdb);
 
-tfbs.plotEnrichment(tfbs, t.comp, file.pdf="enrich-qqplot.pdf", plot.type="nonpolar", enrichment.type="enriched")
+#db <- CisBP.extdata("Homo_sapiens");
+#tfs <- tfbs.createFromCisBP(db);
 
-tfbs.plotEnrichment(tfbs, t.comp, file.pdf="enrich-qqplot.pdf", plot.type="nonpolar", enrichment. type="both")
+load("tfs.rdata");
 
-tfbs.plotEnrichment(tfbs, t.comp, file.pdf="enrich-qqplot.pdf", plot.type="polar", enrichment.type="depleted")
+tfbs.plotEnrichment(tfs, t.comp, file.pdf="enrich-qqplot2.pdf", plot.type="nonpolar", enrichment.type="both", options=list( width=4, zoom.motif.label=1.5, zoom.motif.logo=2, zoom.tick=1, zoom.label=1, zoom.legend.label=1 ))
 
-tfbs.plotEnrichment(tfbs, t.comp, file.pdf="enrich-qqplot.pdf", plot.type="polar", enrichment.type="both")
+tfbs.plotEnrichment(tfs, t.comp, file.pdf="enrich-qqplot1.pdf", plot.type="nonpolar", enrichment.type="enriched")
+
+tfbs.plotEnrichment(tfs, t.comp, file.pdf="enrich-qqplot3.pdf", plot.type="polar", enrichment.type="depleted")
+
+tfbs.plotEnrichment(tfs, t.comp, file.pdf="enrich-qqplot4.pdf", plot.type="polar", enrichment.type="both")
 
